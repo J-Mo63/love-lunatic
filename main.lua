2b42e2706408eb53517b2a164a79fb78cf7560bb
Jonathan Moallem <jdmoallem@gmail.com>
2019-05-05 16:48:23 +1000

    Modularised player functionality

--- main.lua
+++ main.lua
@@ -1,73 +1,38 @@
 -- This file is for playing around with the language
 
-local text = {
-  x  = 100,
-  y  = 100,
-  speed = 4,
-}
-
-local magnitude = 1
+local system = require("system")
+local player = require("player")
 
 function love.update(dt)
-  -- Make the application quit on command + w
-  if love.keyboard.isDown("lgui") and love.keyboard.isDown("w") then
-    love.event.quit()
-  end
-
-  a_x = 0
-  a_y = 0
-
-  if love.keyboard.isDown("right") then
-    a_x = (dt * 100)
-  end
-  if love.keyboard.isDown("left") then
-    a_x = -(dt * 100)
-  end
-  if love.keyboard.isDown("down") then
-    a_y = (dt * 100)
-  end
-  if love.keyboard.isDown("up") then
-    a_y = -(dt * 100)
-  end
-
-  -- Normalise movement
-  magnitude = math.sqrt(a_x^2 + a_y^2)
-  if magnitude > 1 then
-    text.x = text.x + (a_x / magnitude) * text.speed
-    text.y = text.y + (a_y / magnitude) * text.speed
-  end
+  system.update()
+  player.update_movement(dt)
 end
 
 function love.draw()
-  love.graphics.print(magnitude, text.x, text.y, 50)
+  love.graphics.print("0-0", player.location.x, player.location.y, 50)
 end
 
-print("hello, world")
-
-function display_table(table_var)
-  for i, v in ipairs(table_var) do
-    if type(v) == type(table) then
-      print("Found Inner Table:")
-      display_table(v)
-    else
-      print(i, v)
-    end
-  end
-end
-
-test_table = {"hi", {{"hi", {"hi", "hello"}}, "hello"}, "hi", {"hi", {"hi", "hello"}}, key = "val"}
-
-table.remove(test_table, 1)
-
-display_table(test_table)
-
-print(test_table["key"])
 
 
-local more = require("more")
+-- function display_table(table_var)
+--   for i, v in ipairs(table_var) do
+--     if type(v) == type(table) then
+--       print("Found Inner Table:")
+--       display_table(v)
+--     else
+--       print(i, v)
+--     end
+--   end
+-- end
 
-more.run()
+-- test_table = {"hi", {{"hi", {"hi", "hello"}}, "hello"}, "hi", {"hi", {"hi", "hello"}}, key = "val"}
+
+-- table.remove(test_table, 1)
+
+-- display_table(test_table)
+
+-- print(test_table["key"])
 
 
--- Get the last integer key
-print(#{1, 2, 3,1, 3,1, 2, 3})
+-- -- Get the last integer key
+-- print(#{1, 2, 3,1, 3,1, 2, 3})
---
+++ player.lua
@@ -0,0 +1,36 @@
+-- A module for player and player controller related methods and variables
+local M = {}
+
+M.location = {
+  x  = 100,
+  y  = 100,
+}
+
+local player_speed = 4
+
+function M.update_movement(dt)
+  -- Get movement profiles for input
+  temp_x = 0
+  temp_y = 0
+  if love.keyboard.isDown("right") then
+    temp_x = (dt * 100)
+  end
+  if love.keyboard.isDown("left") then
+    temp_x = -(dt * 100)
+  end
+  if love.keyboard.isDown("down") then
+    temp_y = (dt * 100)
+  end
+  if love.keyboard.isDown("up") then
+    temp_y = -(dt * 100)
+  end
+
+  -- Normalise movement
+  magnitude = math.sqrt(temp_x^2 + temp_y^2)
+  if magnitude > 1 then
+    M.location.x = M.location.x + (temp_x / magnitude) * player_speed
+    M.location.y = M.location.y + (temp_y / magnitude) * player_speed
+  end
+end
+ 
+return M
