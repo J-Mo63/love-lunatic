7f079b7790f18459cf4978739ff55b3fe77a5982
Jonathan Moallem <jdmoallem@gmail.com>
2019-07-20 16:59:44 -0400

    Added FPS count option to system module

--- scripts/system.lua
+++ scripts/system.lua
@@ -1,6 +1,10 @@
 -- A module for system related methods and variables
 local M = {}
 
+-- Module Fields
+local display_fps = false
+
+-- Globals
 to_console = "Console ready"
 
 -- Checks for any system commands
@@ -8,6 +12,16 @@ function M.update(dt)
   -- Make the application quit on command + w
   if love.keyboard.isDown("lgui") and love.keyboard.isDown("w") then
     love.event.quit()
+  end
+
+  -- Make the application display FPS on command + f
+  if love.keyboard.isDown("lgui") and love.keyboard.isDown("f") then
+    display_fps = true
+  end
+
+  -- Check if FPS should be displayed
+  if display_fps then
+    to_console = tostring(love.timer.getFPS())
   end
 end
 
