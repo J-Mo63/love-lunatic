c5de3587d5f38c68d18e41af8e0b58ea4a4896ee
Jonathan Moallem <jdmoallem@gmail.com>
2019-08-07 16:53:07 -0400

    Stopped double-checking for command key in system module

--- scripts/system.lua
+++ scripts/system.lua
@@ -7,14 +7,13 @@ debug_mode = false
 
 -- Checks for any system commands
 function M.update(dt)
-  -- Make the application quit on command + w
-  if love.keyboard.isDown("lgui") and love.keyboard.isDown("w") then
-    love.event.quit()
-  end
-
-  -- Make the application display FPS on command + f
-  if love.keyboard.isDown("lgui") and love.keyboard.isDown("d") then
-    debug_mode = true
+  -- Define command functions
+  if love.keyboard.isDown("lgui") then
+    -- Make the application quit on command + w
+    if love.keyboard.isDown("w") then
+      love.event.quit()
+    end
+    
   end
 
   -- Check if FPS should be displayed
