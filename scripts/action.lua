07748d5ce4ae3f54b8c6aeb8b6a665988d73feff
Jonathan Moallem <jdmoallem@gmail.com>
2019-08-30 10:35:21 +1000

    Added test actions to action module

--- scripts/action.lua
+++ scripts/action.lua
@@ -1,6 +1,21 @@
 -- A module for action related methods and variables
 local M = {}
 
+M.actions = {
+  secret_bush = {"print", "dialog", 3}
+}
 
+
+function M.init()
+end
+
+function M.dispatch_action(action)
+  action_instructions = M.actions[action]
+
+  if action_instructions[1] == "print" then
+    if action_instructions[2] == "dialog" then
+    end
+  end
+end
 
 return M
