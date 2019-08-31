daf7556e239387358c6e8676190b1d1a93a68198
Jonathan Moallem <jdmoallem@gmail.com>
2019-08-31 15:33:33 +1000

    Stopped null tag actions from crashing the game

--- scripts/action.lua
+++ scripts/action.lua
@@ -13,10 +13,14 @@ end
 function M.dispatch_action(action)
   action_instructions = M.actions[action]
 
-  if action_instructions[1] == "print" then
-    if action_instructions[2] == "dialog" then
-      to_console = dialog[action_instructions[3]]
+  if action_instructions then
+    if action_instructions[1] == "print" then
+      if action_instructions[2] == "dialog" then
+        to_console = dialog[action_instructions[3]]
+      end
     end
+  else
+    to_console = "null action"
   end
 end
 
