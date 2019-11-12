4f7d95b0067c86eaa7dc1bf5bba1ff75644d1ec2
Jonathan Moallem <jdmoallem@gmail.com>
2019-11-13 01:30:05 +1100

    Added new actions to actions table

--- scripts/action.lua
+++ scripts/action.lua
@@ -2,7 +2,9 @@
 local M = {}
 
 M.actions = {
-  secret_bush = {"print", "dialog", 3}
+  secret_bush = {"print", "dialog", 3},
+  map_2_lower = {"change_map", "map_2", {x = 5, y = 5}},
+  map_2_upper = {"change_map", "map_2", {x = 16, y = 3}},
 }
 
 local dialog = {"broke", "still broke", "worked!"}
@@ -13,15 +15,13 @@ end
 function M.dispatch_action(action)
   action_instructions = M.actions[action]
 
-  if action == "map_2" then
-    setup_scene("map_2", {x = 0, y = 0})
-  end
-  
   if action_instructions then
     if action_instructions[1] == "print" then
       if action_instructions[2] == "dialog" then
         to_console = dialog[action_instructions[3]]
       end
+    elseif action_instructions[1] == "change_map" then
+      setup_scene(action_instructions[2], action_instructions[3])
     end
   else
     to_console = "null action"
