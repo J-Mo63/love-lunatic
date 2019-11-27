d84fb9c472b3d0893958d912e7c254d2d293a68d
Jonathan Moallem <jdmoallem@gmail.com>
2019-11-27 16:40:08 +1100

    Added cooldown to dialogue

--- scripts/dialogue.lua
+++ scripts/dialogue.lua
@@ -1,16 +1,38 @@
 -- A module for dialogue related methods and variables
 local M = {}
 
--- A dialogues table
+-- A dialogue table
 local dialogue_table = {"broke", "still broke", "worked! this motherfucking worked dude!"}
 
-local dialogue = ""
-local max_width = 10
+local dialogue = nil
+local max_width = 20
+local action_cooldown = 0
+
+
+function M.update(dt)
+  if dialogue and love.keyboard.isDown("f") and action_cooldown <= 0 then
+    M.progress_dialogue()
+    action_cooldown = 30
+
+    if dialogue == "" then
+      -- Module.system.control_override = false
+    end
+  end
+
+  if action_cooldown > 0 then
+    action_cooldown = action_cooldown - 1
+  end
+end
 
 -- A method to display dialogue given a dialogue id value
 function M.display_dialogue(dialogue_id)
   -- Display the dialogue on screen
   dialogue = dialogue_table[dialogue_id]
+  Module.system.control_override = true
+end
+
+function M.progress_dialogue()
+  dialogue = string.sub(dialogue, max_width + 1, -1)
 end
 
 function M.render()
